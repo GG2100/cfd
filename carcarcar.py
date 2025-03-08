3d337d7838481d735f137ca0beff4c0818bb8d9c
cffd <pkrystina7@gmail>
2025-03-08 14:42:43 +0200

    kk

--- carcarcar.py
+++ carcarcar.py
@@ -1,3 +1,21 @@
 print("gfghfhg")
 print("france")
-jkh
+
+num1=float(input("num1= "))
+num2=float(input("num2= "))
+operation=input("operation= ")
+def calc(num1,num2,operation):
+    if operation == "+":
+        return num1+num2
+    elif operation == "-":
+        return num1-num2
+    elif operation == "*":
+        return num1*num2
+    elif operation == "/":
+        return num1/num2
+    else:
+        print("Error")
+
+print(calc(num1,num2,operation))
+
+
