c0aef87d9cb6d493c362a410a4b4de1969c78a43
cffd <pkrystina7@gmail>
2025-03-15 01:05:56 +0200

    L

--- carcarcar.py
+++ carcarcar.py
@@ -4,18 +4,26 @@ print("france")
 num1=float(input("num1= "))
 num2=float(input("num2= "))
 operation=input("operation= ")
-def calc(num1,num2,operation):
-    if operation == "+":
-        return num1+num2
-    elif operation == "-":
-        return num1-num2
-    elif operation == "*":
-        return num1*num2
-    elif operation == "/":
-        return num1/num2
-    else:
-        print("Error")
+while True:
+    def calc(num1, num2, operation):
+        if operation == "+":
+            return num1 + num2
+        elif operation == "-":
+            return num1 - num2
+        elif operation == "*":
+            return num1 * num2
+        elif operation == "/":
+            return num1 / num2
+        else:
+            print("Error")
 
-print(calc(num1,num2,operation))
+    print(calc(num1, num2, operation))
+
+    if operation == "0":
+        print("Error")
+    break
+
+
+
 
 
