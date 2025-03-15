22e3a83ed6c8fc38ef8c211b35b185cc596e7189
cffd <pkrystina7@gmail>
2025-03-15 10:54:54 +0200

    oo

--- carcarcar.py
+++ carcarcar.py
@@ -1,10 +1,12 @@
 print("gfghfhg")
 print("france")
 
-num1=float(input("num1= "))
-num2=float(input("num2= "))
-operation=input("operation= ")
+
+
 while True:
+    num1 = float(input("num1= "))
+    num2 = float(input("num2= "))
+    operation = input("operation= ")
     def calc(num1, num2, operation):
         if operation == "+":
             return num1 + num2
@@ -13,16 +15,20 @@ while True:
         elif operation == "*":
             return num1 * num2
         elif operation == "/":
+            num2 != 0
             return num1 / num2
-        else:
+        elif operation == "/":
+            num2 == 0
             print("Error")
 
     print(calc(num1, num2, operation))
 
     if operation == "0":
         print("Error")
-    break
-g
+        break
+
+
+
 
 
 
